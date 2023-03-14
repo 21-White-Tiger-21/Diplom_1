69a777ab310d3d9aef1dc9132466c31ff43ad8ae
Vitaly Matveev <vitalya.matv3ev@yandex.ru>
2023-03-14 09:57:06 +0300

    BunTest update

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,17 +1,30 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
-
-
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Parameterized.class)
 public class BunTest {
+    private final String name;
+    private final float price;
     private Bun bun;
-
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+    @Parameterized.Parameters(name = "Тестовые данные: {0} {1}")
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"black bun", 10},
+                {"", 0},
+                {null, -10},
+                {"Miracle_Long_Bun_Miracle_Long_Bun", 3.4E+38f},
+                {"856492", 40.123456f},
+                {"!@#$%^&*()", 4.7E-23f},
+        };
+    }
     @Before
     public void createNewInstance() {
         bun = new Bun("original", 200.0f);
